da5537a1dc39b6cda4af613cfa9390069c06c32b
Matt Pedersen <mattpp@Matts-MacBook-Air.local>
2017-03-07 14:28:01 -0800

    hihi

--- game.js
+++ game.js
@@ -21,34 +21,33 @@ Game.prototype.dealCards = function() {
   this.dealer.showHand($('.dealer'))
 
   $('.actions').html(`<button class="hit">Hit</button><button class="stay">Stay</button>`)
+
   $('.hit').on('click', function() {
-    if (self.playerScore > 21) {
-      console.log('YOU LOOOOOOOOOSE')
-    }
-    else {
-      console.log('DEALING ANOTHER CARRDDDDDDDD')
-      self.player.draw(self.deck)
-      self.player.showHand($('.player'))
-      self.getCardTotal('player')
-    }
+    self.checkForBust(self.player.hand)
   })
-  $('.stay')
+
+  // $('.stay')
 
   this.getCardTotal('player')
   this.getCardTotal('dealer')
 }
 
 Game.prototype.getCardTotal = function(who) {
+  // get the dealer total of only face up card ( during player's turn )
   if (who == 'dealer' && this.whoseTurn == 'player') {
     this.dealerScore += this.dealer.hand[1].weight
     $('.dealer-card-total').html(this.dealerScore)
   }
+
+  // get the dealer total if its their turn
   if (who == 'dealer' && this.whoseTurn == 'dealer') {
     for (var i = 0; i < this.dealer.hand.length; i++) {
       this.dealerScore += this.dealer.hand[i].weight
     }
     $('.dealer-card-total').html(this.dealerScore)
   }
+
+  // get the players total
   if (who == 'player') {
     var score = 0
     for (var i = 0; i < this.player.hand.length; i++) {
@@ -59,4 +58,26 @@ Game.prototype.getCardTotal = function(who) {
   }
 }
 
-Game.prototype.checkForBust
+Game.prototype.checkForBust = function(hand, aces, total) {
+  if(!aces || ! total){
+    var aces = [];
+    var total = 0;
+  }
+  for(var i = 0; i< hand.length; i++){
+    if (hand[i].weight == 11){
+      aces.push(i);
+    }
+    total += hand[i].total;
+  }
+  if(this.total > 21 && aces.length == 0){
+    return this.total;
+  }
+  else if(this.total < 21){
+    return this.total;
+  }
+  else{
+    hand[aces[aces.length-1]].weight = 1;
+    aces.pop();
+    return(checkForBust(hand, aces, total));
+  }
+}
